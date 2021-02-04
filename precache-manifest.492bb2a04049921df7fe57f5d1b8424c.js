42e6ab1c0be5d5e1082c8b154d810bc2a93f45ae
vladimir kutysh <webexpert0524@gmail.com>
2021-02-05 02:34:22 +0300

    frontend1

---
+++ precache-manifest.492bb2a04049921df7fe57f5d1b8424c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f096d0a3cf9bacd975b",
+    "url": "/static/css/main.c59ca1ac.chunk.css"
+  },
+  {
+    "revision": "5f096d0a3cf9bacd975b",
+    "url": "/static/js/main.a7d8e214.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a75bbdd0044f32e3b033",
+    "url": "/static/css/2.ad03ce01.chunk.css"
+  },
+  {
+    "revision": "a75bbdd0044f32e3b033",
+    "url": "/static/js/2.2d5dd2f7.chunk.js"
+  },
+  {
+    "revision": "5f2481dec2b2a7d3788bc9fd319c232b",
+    "url": "/index.html"
+  }
+];
